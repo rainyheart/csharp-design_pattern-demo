bd4854ea5752a80375c0672703cf4f2dc61b6d18
Ken Ye <yjz_work@126.com>
2019-03-12 16:45:35 +0800

    Init project

---
+++ DesignPatterns/DesignPatterns/src/Ken/Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.src
+{
+    class Singleton
+    {
+        private static Singleton _Singleton = null;
+        private static object SingletonLock = new object();
+        public static Singleton CreateInstance()
+        {
+            if (_Singleton == null)
+            {
+                lock (SingletonLock)
+                {
+                    if (_Singleton == null)
+                    {
+                        Console.WriteLine("Singleton is created");
+                        _Singleton = new Singleton();
+                    }
+                }
+            }
+            return _Singleton;
+        }
+    }
+}
---
+++ DesignPatterns/DesignPatterns/test/TestSingleton.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using DesignPatterns.src;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace DesignPatterns.test
+{
+    [TestClass]
+    public class TestSingleton
+    {
+        [TestMethod]
+        public void CreateSingleton()
+        {
+            var taskFactory = new TaskFactory();
+            var taskList = new List<Task>();
+            var taskSet = new HashSet<Singleton>();
+
+            for (var i = 0; i < 5; i++)
+            {
+                taskList.Add(taskFactory.StartNew(() =>
+                {
+                    var singleton = Singleton.CreateInstance();
+                    Console.WriteLine(singleton.GetHashCode());
+                    taskSet.Add(singleton);
+                }));
+            }
+
+            Thread.Sleep(2000);
+            Console.WriteLine(taskSet.Count);
+            
+        }
+    }
+}
